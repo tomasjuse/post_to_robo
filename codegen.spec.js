ce844bf1522b7e1233be4bb55542d4fdfd923851
Tomas Juselius <tomas.juselius@vincit.fi>
2025-11-25 16:00:47 +0200

    small update to find exact match on page

--- codegen.spec.js
+++ codegen.spec.js
@@ -11,4 +11,13 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Input: Password' }).fill('pass');
   await page.getByRole('textbox', { name: 'Input: Date' }).fill('2025-11-26');
   await page.getByRole('button', { name: 'Display Inputs' }).click();
+
+  // following added after codegen to verify input
+  await page.waitForTimeout(3000);
+  await expect(page.getByText('pass', { exact: true })).toBeVisible();
+  await expect(page.getByText('88')).toBeVisible();
+  await expect(page.getByText('hepp')).toBeVisible();
+  await expect(page.getByText('2025-11-26')).toBeVisible();
+  await page.screenshot({ path: 'playwright-screenshot.png' });
+
 });
